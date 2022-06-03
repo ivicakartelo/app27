bdd6bbfaaa44824958f74a4d127fe3d2d806ed4d
ivicakartelo <ivicakartelo@users.noreply.github.com>
2022-06-03 22:55:08 +0200

    Compere to pure HTML

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Products from './components/Products';
+import Testimony from './components/Testimony';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/products">Products</Link></li>
+        <li><Link to="/testimony">Testimony</Link></li>
+      </ul>
+      
+        <div>
+          <Switch>
+            <Route path="/" component={Home} exact /> 
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />     
+            <Route path="/products" component={Products} />
+            <Route path="/testimony" component={Testimony} />
+          </Switch>            
+        </div>            
+      </Router>
   );
 }
-
 export default App;
